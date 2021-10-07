895741150c3e1607c8c16b39d1ef791a248b42d0
me11y <imelon2013@gmail.com>
2021-10-07 10:02:46 +0300

    [+] scene objects [+] TowerBuilder [+] Block prefab

---
+++ Assets/Scripts/Tower/TowerBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBuilder : MonoBehaviour
+{
+    [SerializeField] private float _towerSize;
+    [SerializeField] private Transform _buildPoint;
+    [SerializeField] private Block _block;
+
+    private readonly Queue<Block> _blocks = new Queue<Block>();
+
+    private void Awake()
+    {
+        Build();
+    }
+
+    private void Build()
+    {
+        Transform currentPoint = _buildPoint;
+
+        for (int i = 0; i < _towerSize; i++)
+        {
+            Block newBlock = BuildBlock(currentPoint);
+            _blocks.Enqueue(newBlock);
+            currentPoint = newBlock.transform;
+        }
+    }
+
+    private Block BuildBlock(Transform currentBuildPoint)
+    {
+        return Instantiate(_block, GetBuildPoint(currentBuildPoint), Quaternion.identity, _buildPoint);
+    }
+
+    private Vector3 GetBuildPoint(Transform currentSegment)
+    {
+        return new Vector3(_buildPoint.position.x, currentSegment.position.y + currentSegment.localScale.y / 2 + _block.transform.localScale.y / 2, _buildPoint.position.z);
+    }
+}
