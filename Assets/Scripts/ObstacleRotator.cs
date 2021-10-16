16140a5d5bcb847c302bf52bc2cf4beeb36b208c
me11y <imelon2013@gmail.com>
2021-10-16 17:49:40 +0300

    [+] 60deg obstacle [+] obstacle rotation

---
+++ Assets/Scripts/ObstacleRotator.cs
@@ -0,0 +1,12 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class ObstacleRotator : MonoBehaviour
+{
+    [SerializeField] private float _rotationDuration;
+
+    private void Awake()
+    {
+        transform.DORotate(new Vector3(0, 360, 0), _rotationDuration, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Yoyo);
+    }
+}
